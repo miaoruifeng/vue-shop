3ca6fe15181b4b65efd794878b0821f430f18000
miaoruifeng <mrfnet@163.com>
2019-11-24 22:58:05 +0800

    'add-login'

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,7 @@
 <template>
   <div id="app">
-    <el-row>
-      <el-button>默认按钮</el-button>
-      <el-button type="primary">主要按钮</el-button>
-      <el-button type="success">成功按钮</el-button>
-      <el-button type="info">信息按钮</el-button>
-      <el-button type="warning">警告按钮</el-button>
-      <el-button type="danger">危险按钮</el-button>
-    </el-row>
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <router-view></router-view>
   </div>
 </template>
 
-<style lang="stylus">
-#app
-  font-family 'Avenir', Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
-</style>
+<style lang="stylus"></style>
---
+++ src/components/Login.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="login_container">
+    <div class="login_box">
+      <div class="avatar_box">
+        <img src="../assets/logo.png" />
+      </div>
+      <el-form class="login-form">
+        <el-form-item>
+          <el-input></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input></el-input>
+        </el-form-item>
+        <el-form-item class="btns">
+          <el-button type="primary">登录</el-button>
+          <el-button type="info">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login'
+}
+</script>
+
+<style lang="less" scoped>
+  .login_container {
+    height: 100%;
+    background-color: #2b4b6b;
+  }
+  .login_box {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 450px;
+    height: 300px;
+    background-color: #fff;
+    border-radius: 3px;
+    .avatar_box {
+      position: absolute;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      width: 130px;
+      height: 130px;
+      padding: 10px;
+      background-color: #fff;
+      border: 1px solid #eee;
+      border-radius: 50%;
+      box-shadow: 0 0 10px #ddd;
+      img {
+        width: 100%;
+        height: 100%;
+        border-radius: 50%;
+        background-color: #eee;
+      }
+    }
+  }
+  .login-form {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    padding: 0 40px;
+    box-sizing: border-box;
+  }
+  .btns {
+    display: flex;
+    justify-content: flex-end;
+  }
+</style>
